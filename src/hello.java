7f3aa57237596c64b1a80ca0fdb12911cea431c7
Allauddin Patel <94365044+AllauddinPatel@users.noreply.github.com>
2021-11-15 17:31:40 +0530

    hi

--- src/hello.java
+++ src/hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello1");
     }
 }
 //https://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@github.com/AllauddinPatel/firstprogram.git
---
+++ src/hello1/hi.java
@@ -0,0 +1,7 @@
+package hello1;
+
+public class hi {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
