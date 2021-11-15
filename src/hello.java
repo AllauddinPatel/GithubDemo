b2feafe929512d6038c4482d613dd5109421c00c
AllauddinPatel <patelallauddin53@gmail.com>
2021-11-15 17:03:51 +0530

    hello

---
+++ src/hello.java
@@ -0,0 +1,6 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
+//https://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@github.com/AllauddinPatel/firstprogram.git
